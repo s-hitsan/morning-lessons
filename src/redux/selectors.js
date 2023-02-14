b014fae3688cfe61967259739e799350f083d159
s-hitsan <vasyliohloserhii@gmail.com>
2023-02-14 11:37:06 +0200

    createSelector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,9 +1,11 @@
-export const selectNotesPage = (state) => {
-  const { notes, searchString } = state.notesPage;
-  const filteredNotes = notes.filter((el) =>
-    el.tittle.toLowerCase().includes(searchString.toLowerCase()),
-  );
-  console.log(filteredNotes);
+import { createSelector } from '@reduxjs/toolkit';
+
+export const notesPage = (state) => state.notesPage;
+
+export const selectNotesPage = createSelector([notesPage], ({ notes, searchString }) => {
+  const filteredNotes = notes?.filter((el) => {
+    return el.tittle.toLowerCase().includes(searchString.toLowerCase());
+  });
 
   return { notes: filteredNotes, searchString };
-};
+});
